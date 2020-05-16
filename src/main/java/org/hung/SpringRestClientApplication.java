f7e9ae7a553a38494ab42e0e130b71a7bb80aabf
Kwong-Hung YIP <kwonghung.yip@gmail.com>
2020-05-17 03:31:57 +0800

    fixed trust self signd issue

--- src/main/java/org/hung/SpringRestClientApplication.java
+++ src/main/java/org/hung/SpringRestClientApplication.java
@@ -1,10 +1,28 @@
 package org.hung;
 
+import java.io.File;
+import java.io.IOException;
+import java.security.KeyManagementException;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.security.cert.CertificateException;
+
+import javax.net.ssl.SSLContext;
+
+import org.apache.http.client.HttpClient;
+import org.apache.http.conn.ssl.NoopHostnameVerifier;
+import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
+import org.apache.http.conn.ssl.TrustAllStrategy;
+import org.apache.http.conn.ssl.TrustSelfSignedStrategy;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.ssl.SSLContextBuilder;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.client.ClientHttpRequestFactory;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 
 import lombok.extern.slf4j.Slf4j;
@@ -18,12 +36,44 @@ public class SpringRestClientApplication {
 	}
 
 	@Bean
-	CommandLineRunner runRestClient(RestTemplateBuilder builder) {
+	CommandLineRunner runRestClient(
+			RestTemplateBuilder builder
+	) {
 		return (args) -> {
 			log.info("Run RestTemplate client");
-			RestTemplate client = builder.build();
 			
-			String result = client.getForObject("https://localhost:8080/echo", String.class);
+			RestTemplate restTemplate = builder
+			.additionalCustomizers((template) -> {
+				SSLContext sslContext = null;
+				try {
+
+					File trustStore = new File("C:/Users/kwong/Documents/certs/selfsigned/ca/trust.jks");
+					sslContext = SSLContextBuilder.create()
+						.loadTrustMaterial(trustStore,"abcd1234".toCharArray())
+						//.loadTrustMaterial(new TrustAllStrategy())
+						//.loadTrustMaterial(new TrustSelfSignedStrategy())	
+						.build();
+				} catch ( KeyManagementException | NoSuchAlgorithmException | KeyStoreException | CertificateException | IOException  e) {
+					log.error("{}",e);
+				}
+				
+				SSLConnectionSocketFactory socketFactory = new SSLConnectionSocketFactory(sslContext,NoopHostnameVerifier.INSTANCE);
+				
+				HttpClient httpClient = HttpClientBuilder.create()
+						.setSSLSocketFactory(socketFactory)
+						//.useSystemProperties()
+						.build();
+				
+				ClientHttpRequestFactory requestFactory = new HttpComponentsClientHttpRequestFactory(httpClient);
+				
+				template.setRequestFactory(requestFactory);
+			})
+			.build();
+			
+			
+			String result = restTemplate.getForObject("https://localhost:8080/echo", String.class);
+			
+			log.info("result: {}",result);
 		};
 	}
 }
